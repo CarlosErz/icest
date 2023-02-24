4bbdf65f2ce34441dd0ec4467ada520402ea6de2
CarlosErz <rosariosalda25@gmail.com>
2023-02-24 15:15:31 -0600

    bg moviendose en test

--- test.py
+++ test.py
@@ -9,34 +9,39 @@ FPS = 60
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption(title)
 scroll = 0 
+scroll_speed = 5 # velocidad de desplazamiento del fondo
 bg_images = []
 for i in range(1, 5):
     bg_image = pygame.image.load(f"sprite/bg_{i}.png").convert_alpha()
     bg_images.append(bg_image)
 
+def draw_bg(window, scroll):
+    new_width = window.get_width()
+    new_height = window.get_height()
+    scaled_images = [pygame.transform.scale(
+        i, (new_width, new_height)) for i in bg_images]
 
-
-def draw_bg():
     for x in range(5):
-        for i in bg_images:
-            new_width = window.get_width()
-            new_height = window.get_height()
-            scaled_image = pygame.transform.scale(i, (new_width, new_height))
-            window.blit(scaled_image, ((x* new_width) -scroll, 0))
+        for i, scaled_image in enumerate(scaled_images):
+            # calcular la posición horizontal de la imagen de fondo
+            pos_x = x * new_width - (scroll % new_width)
+
+            # dibujar la imagen de fondo en la posición calculada
+            window.blit(scaled_image, (pos_x, 0))
+
 run = True
 while run:
-
     clock.tick(FPS)
-    draw_bg()
-    key=pygame.key.get_pressed()
-    if key[pygame.K_LEFT]:
-        scroll -=5
-    if key[pygame.K_RIGHT]:
-        scroll +=5 
+
+    # Actualizar la variable 'scroll' para mover el fondo
+    scroll += scroll_speed
+
+    draw_bg(window,scroll)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     pygame.display.update()
+
 pygame.quit()
